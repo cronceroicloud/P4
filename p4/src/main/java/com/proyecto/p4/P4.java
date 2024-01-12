e08a6aa2e223ac5f077356a65b9bcea7f456f0e0
cronceroicloud <croncero@icloud.com>
2024-01-12 10:44:58 +0100

    Commit2: Añadir p4.java y pom.xml

--- p4/src/main/java/com/proyecto/p4/P4.java
+++ p4/src/main/java/com/proyecto/p4/P4.java
@@ -13,5 +13,6 @@ public class P4 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Adios");
+        System.out.println("Adios2");
     }
 }
